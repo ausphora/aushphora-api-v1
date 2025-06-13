6ba439219a02d90e980fabe39cbbe6b5347600b2
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-06-13 16:52:28 +0530

    avatar upload folder name fixed for cloudinary

--- src/controllers/avatarControllers.ts
+++ src/controllers/avatarControllers.ts
@@ -36,7 +36,7 @@ export const uploadAvatar = [
 
             // Upload to Cloudinary
             const cloudinaryResponse = await cloudinary.uploader.upload(dataURI, {
-                folder: 'letmerecall_avatar_uploads'
+                folder: 'ausphoratest_avatar_uploads'
             });
 
             // First, deleting any existing avatar for this user 
